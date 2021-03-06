551ff326a5d636b5acb6f2cb7bea22fe286651dc
inocchii <inocchii@gmail.com>
2021-03-06 16:46:48 +0900

    ニフクラのアプリキーを設定

--- daily_report/www/js/app.js
+++ daily_report/www/js/app.js
@@ -1,3 +1,3 @@
-const applicationKey = 'YOUR_APPLICATION_KEY';
-const clientKey = 'YOUR_CLIENT_KEY';
+const applicationKey = 'dff6fdb302680d70067bc8c93ef363f269b5616330878ecd57a1fa4fc14e3eed';
+const clientKey = '5924bd6deecff53942cdb7a9ffef0c6c367db6df0a58c26ab9b9472f235b2e30';
 const ncmb = new NCMB(applicationKey, clientKey);
