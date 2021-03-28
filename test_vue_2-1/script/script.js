120be17e90a9b80b9a4108c3a8e153be096cbb53
inokuchi <>
2021-03-28 12:18:33 +0900

    Vue testing

--- test_vue_2-1/script/script.js
+++ test_vue_2-1/script/script.js
@@ -1,8 +1,35 @@
-new Vue({
-    el: '#app', // el: Vueインスタンスを結びつけるHTML要素を指定。この要素配下のみでVue.jsが有効となる
-    data(){     // data: function(){ という書き方が基本 => data(){ は短縮形
-      return {
-        text: 'hello Vue.js'
-      }
+// Vue sample
+//   View  : UI＆見た目の部分（HTML）
+//   Action: Viewからのイベント処理（methods）
+//   State : 状態、データを保持（dataプロパティ）
+const objVue = new Vue({
+    // el: Vueのインスタンスを結びつけるHTML要素を指定 = View
+    el: '#app',
+    // data: dataプロパティ（data関数） = State
+    data(){
+        return {
+            text1: 'hello Vue.js',
+            text2: 'goodbye jQuery',
+            count: 0
+        }
+    },
+    // methods: メソッド定義 = Action
+    methods: {
+        handleClick() {
+            this.text2 += " clicked!"
+        },
+        increment() {
+            this.count++;
+        },
+        decrement() {
+            this.count--;
+        }
     }
+    // computed: 算出プロパティ（関数）
+    // template: HTMLテンプレート
+    // components: 外部モジュールとして取り込んだコンポーネントを登録
 });
+
+console.log("text1="+objVue.text1+" text2="+objVue.text2);
+objVue.text2+=" and React";
+console.log("text2="+objVue.text2);
